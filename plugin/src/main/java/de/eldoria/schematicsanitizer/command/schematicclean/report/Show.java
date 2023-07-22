92915b0868c318f25e270268ca3440d50a37b168
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-07-22 18:13:19 +0200

    Add error handling for non-existing report in SchematicClean
    This commit adds error handling for situations where a user tries to view a schematic clean report that does not exist yet. Previously, the program would crash if a user tried to view a report that was not yet generated, resulting in poor user experience and potential loss of data. Now, the program interrupts the command execution and informs the user that no report is available, thus preventing potential crashes and improving user experience.

--- plugin/src/main/java/de/eldoria/schematicsanitizer/command/schematicclean/report/Show.java
+++ plugin/src/main/java/de/eldoria/schematicsanitizer/command/schematicclean/report/Show.java
@@ -12,6 +12,7 @@ import de.eldoria.eldoutilities.commands.command.util.Arguments;
 import de.eldoria.eldoutilities.commands.exceptions.CommandException;
 import de.eldoria.eldoutilities.commands.executor.ITabExecutor;
 import de.eldoria.schematicsanitizer.command.schematicclean.Report;
+import de.eldoria.schematicsanitizer.rendering.ReportRenderer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
@@ -27,6 +28,10 @@ public class Show extends AdvancedCommand implements ITabExecutor {
 
     @Override
     public void onCommand(@NotNull CommandSender sender, @NotNull String alias, @NotNull Arguments args) throws CommandException {
-        messageSender().sendMessage(sender, report.get(sender).component());
+        ReportRenderer renderer = report.get(sender);
+        if (renderer == null) {
+            throw CommandException.message("No report available");
+        }
+        messageSender().sendMessage(sender, renderer.component());
     }
 }
